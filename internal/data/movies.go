71520c53be63bcdf5c379f8198921278d2e208f0
Infamous003 <arfathsyed0312@gmail.com>
2025-11-24 17:37:36 +0530

    feat: add a custom Runtime type for runtime

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,13 +4,13 @@ import "time"
 
 type Movie struct {
 	ID          int64     `json:"id"`
-	CreatedAt   time.Time `json:"created_at"`
+	CreatedAt   time.Time `json:"-"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
-	Runtime     int32     `json:"runtime"`
+	Runtime     Runtime   `json:"runtime"`
 	Year        int32     `json:"year"`
 	Genres      []string  `json:"genres"`
 	Languages   []string  `json:"languages"`
 	Version     int32     `json:"version"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	UpdatedAt   time.Time `json:"-"`
 }
